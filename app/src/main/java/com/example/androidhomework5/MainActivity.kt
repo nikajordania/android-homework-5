50d8550239e42a3955431223755844cd442e8199
Nika Zhordania <nika.zhordania.1@btu.edu.ge>
2022-04-09 23:53:01 +0400

    fix async func

--- app/src/main/java/com/example/androidhomework5/MainActivity.kt
+++ app/src/main/java/com/example/androidhomework5/MainActivity.kt
@@ -1,59 +1,66 @@
 package com.example.androidhomework5
 
+import android.os.AsyncTask
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.room.Room
 import com.example.androidhomework5.data.Distance
 import com.example.androidhomework5.data.DistanceDatabase
 
 class MainActivity : AppCompatActivity() {
-    private val db = getDatabase()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val saveButton: Button = findViewById<View>(R.id.save_button) as Button
-
-
+        val saveButton = findViewById<Button>(R.id.save_button)
 
         saveButton.setOnClickListener {
             val distanceRecord = Distance(
-                0,
                 findViewById<EditText>(R.id.run_distance).text.toString().toInt(),
                 findViewById<EditText>(R.id.swim_distance).text.toString().toInt(),
                 findViewById<EditText>(R.id.calories_taken).text.toString().toInt()
             )
 
-            db.distanceDao().insertAll(distanceRecord)
+            saveDistance(distanceRecord)
             avg()
         }
     }
 
+
+    private fun saveDistance(distance: Distance) {
+        class SaveDistance : AsyncTask<Void, Void, Void>() {
+            override fun doInBackground(vararg p0: Void?): Void? {
+                DistanceDatabase(applicationContext!!).distanceDao().insertAll(distance)
+                return null
+            }
+
+            override fun onPostExecute(result: Void?) {
+                Toast.makeText(applicationContext, "Distance Save", Toast.LENGTH_LONG).show()
+            }
+        }
+        SaveDistance().execute()
+    }
+
     private fun avg() {
-        val allRecords: List<Distance> = db.distanceDao().getAll()
+        Thread(Runnable {
+            val allRecords: List<Distance> =
+                DistanceDatabase(applicationContext!!).distanceDao().getAll()
 
-        val fullRunDistance = allRecords.mapNotNull { it.runDistance }.sum().toString()
-        val avgOfRunDistance = allRecords.mapNotNull { it.runDistance }.average().toString()
-        val avgOfSwimDistance = allRecords.mapNotNull { it.swimDistance }.average().toString()
-        val avgOfCalorieTaken = allRecords.mapNotNull { it.calorieTaken }.average().toString()
+            runOnUiThread {
+                val fullRunDistance = allRecords.mapNotNull { it.runDistance }.sum().toString()
+                val avgOfRunDistance = allRecords.mapNotNull { it.runDistance }.average().toString()
+                val avgOfSwimDistance = allRecords.mapNotNull { it.swimDistance }.average().toString()
+                val avgOfCalorieTaken = allRecords.mapNotNull { it.calorieTaken }.average().toString()
 
-        findViewById<TextView>(R.id.run_distance_avg).text = avgOfRunDistance
-        findViewById<TextView>(R.id.swim_distance_avg).text = avgOfSwimDistance
-        findViewById<TextView>(R.id.calories_taken_avg).text = avgOfCalorieTaken
+                findViewById<TextView>(R.id.run_distance_avg).text = avgOfRunDistance
+                findViewById<TextView>(R.id.swim_distance_avg).text = avgOfSwimDistance
+                findViewById<TextView>(R.id.calories_taken_avg).text = avgOfCalorieTaken
 
-        findViewById<TextView>(R.id.run_distance_sum).text = fullRunDistance
+                findViewById<TextView>(R.id.run_distance_sum).text = fullRunDistance
+            }
+        }).start()
     }
-
-    private fun getDatabase(): DistanceDatabase {
-        return Room.databaseBuilder(
-            applicationContext,
-            DistanceDatabase::class.java,
-            "distance"
-        ).build()
-    }
-
 }
--- app/src/main/java/com/example/androidhomework5/data/Distance.kt
+++ app/src/main/java/com/example/androidhomework5/data/Distance.kt
@@ -6,8 +6,10 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Distance(
-    @PrimaryKey(autoGenerate = true) val uid: Int,
     @ColumnInfo(name = "run_distance") val runDistance: Int?,
     @ColumnInfo(name = "swim_distance") val swimDistance: Int?,
     @ColumnInfo(name = "calorie_taken") val calorieTaken: Int?
-)
+) {
+    @PrimaryKey(autoGenerate = true)
+    var uid: Int = 0
+}
